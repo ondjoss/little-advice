2ec76e65b52d6933cbf36e402e1a20abada68d6f
ondjoss <ghislain.chakam@gmail.com>
2022-06-27 16:47:48 +0200

    Modification de la hauteur du heading

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component{
             <div className="app">
                 <div className="card">
                     <h1 className='heading'>{advice}</h1>
-                    <button className='button'>
+                    <button className='button' onClick={this.fetchAdvice}>
                         <span>Give me advice !</span>
                     </button>
                 </div>
